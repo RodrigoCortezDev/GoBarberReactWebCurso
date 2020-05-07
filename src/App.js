447885afc1dadd96a1d14e7788856d31469e63e3
RodrigoCortezDev <rodrigocortezdev@gmail.com>
2020-05-07 01:51:59 -0300

    Iniciando projeto

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css'
+
+import profile from './assets/Perfil.png';
+
+function App() {
+    return <img width='300' src={profile} />
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,15 @@ module.exports={
             { 
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
+                use: { loader: 'babel-loader' }
+            },
+            {
+                test: /\.css$/,
+                use: [ { loader: 'style-loader' }, { loader: 'css-loader' }, ]
+            },
+            {
+                test: /.*\.(gif|png|jpe?g)$/i,
+                use: { loader: 'file-loader' },
             }
         ]
     }
