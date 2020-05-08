2f5eba55c35bf973462276a5a24fe35804c82f57
RodrigoCortezDev <rodrigocortezdev@gmail.com>
2020-05-08 01:32:16 -0300

    Nova Aula

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css'
+import TechList from './components/TechList';
 
 import profile from './assets/Perfil.png';
 
 function App() {
-    return <img width='300' src={profile} />
+    //return <img width='300' src={profile} />
+    return <TechList />
 }
 
 export default App;
